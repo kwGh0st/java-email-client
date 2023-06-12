23f35aad38d3c254392a3a45361e4595e13594e6
Karol Programuje <karol341654@gmail.com>
2023-06-12 13:27:51 +0200

    Implementing login method.

--- src/main/java/app/emailclient/controller/services/LoginService.java
+++ src/main/java/app/emailclient/controller/services/LoginService.java
@@ -1,7 +1,10 @@
 package app.emailclient.controller.services;
 
 import app.emailclient.EmailManager;
+import app.emailclient.controller.EmailLoginResult;
 import app.emailclient.model.EmailAccount;
+
+import javax.mail.*;
 
 public class LoginService {
     private EmailManager emailManager;
@@ -12,4 +15,29 @@ public class LoginService {
         this.emailAccount = emailAccount;
     }
 
+    public EmailLoginResult login() {
+        Authenticator authenticator = new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(emailAccount.getName(), emailAccount.getPassword());
+            }
+        };
+
+        try {
+            Session session = Session.getInstance(emailAccount.getProperties(), authenticator);
+            Store store = session.getStore("imaps");
+            store.connect(emailAccount.getProperties().getProperty("incomingHost"), emailAccount.getName(), emailAccount.getPassword());
+            emailAccount.setStore(store);
+        } catch (AuthenticationFailedException ex) {
+            ex.printStackTrace();
+            return EmailLoginResult.FAILED_BY_CREDENTIALS;
+        } catch (NoSuchProviderException ex) {
+            ex.printStackTrace();
+            return EmailLoginResult.FAILED_BY_NETWORK;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return EmailLoginResult.UNEXPECTED_ERROR;
+        }
+        return EmailLoginResult.SUCCESS;
+    }
 }

e57fa52ce9b47a96e9212cd2872b77e9ca898af9
Karol Programuje <karol341654@gmail.com>
2023-06-15 12:44:39 +0200

    Setting up TableView.

--- src/main/java/app/emailclient/controller/MainWindowController.java
+++ src/main/java/app/emailclient/controller/MainWindowController.java
@@ -1,21 +1,28 @@
 package app.emailclient.controller;
 
 import app.emailclient.EmailManager;
+import app.emailclient.model.EmailMessage;
+import app.emailclient.model.EmailTreeItem;
 import app.emailclient.view.ViewFactory;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TreeView;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.web.WebView;
 
 import java.net.URL;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 public class MainWindowController extends BaseController implements Initializable {
     @FXML
-    private TableView<?> emailTableView;
+    private TableColumn<Date, SimpleObjectProperty<Date>> dateColumn;
+
+    @FXML
+    private TableView<EmailMessage> emailTableView;
 
     @FXML
     private TreeView<String> emailTreeView;
@@ -27,7 +34,20 @@ public class MainWindowController extends BaseController implements Initializabl
     private MenuBar menuBar;
 
     @FXML
+    private TableColumn<String, SimpleStringProperty> recipientColumn;
+
+    @FXML
+    private TableColumn<String, SimpleStringProperty> senderColumn;
+
+    @FXML
+    private TableColumn<Integer, SimpleIntegerProperty> sizeColumn;
+
+    @FXML
+    private TableColumn<String, SimpleStringProperty> subjectColumn;
+
+    @FXML
     private MenuItem addAccountButton;
+
 
     public MainWindowController(ViewFactory viewFactory, EmailManager emailManager, String fxmlName) {
         super(viewFactory, emailManager, fxmlName);
@@ -46,6 +66,26 @@ public class MainWindowController extends BaseController implements Initializabl
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         setUpEmailsTreeView();
+        setUpTableView();
+        setUpFolderSelection();
+    }
+
+    private void setUpTableView() {
+        subjectColumn.setCellValueFactory(new PropertyValueFactory<String, SimpleStringProperty>("subject"));
+        recipientColumn.setCellValueFactory(new PropertyValueFactory<String, SimpleStringProperty>("recipient"));
+        senderColumn.setCellValueFactory(new PropertyValueFactory<String, SimpleStringProperty>("sender"));
+        sizeColumn.setCellValueFactory(new PropertyValueFactory<Integer, SimpleIntegerProperty>("size"));
+        dateColumn.setCellValueFactory(new PropertyValueFactory<Date, SimpleObjectProperty<Date>>("date"));
+    }
+
+    private void setUpFolderSelection() {
+        emailTableView.setOnMouseClicked(event -> {
+            EmailTreeItem<String> item = (EmailTreeItem<String>) emailTreeView.getSelectionModel().getSelectedItem();
+
+            if (item != null) {
+                emailTableView.setItems(item.getEmailMessages());
+            }
+        });
     }
 
     private void setUpEmailsTreeView() {

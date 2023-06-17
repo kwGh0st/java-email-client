74455cd538d399ef1e5275b9d2ff29a1d37a66cd
Karol Programuje <karol341654@gmail.com>
2023-06-17 14:10:31 +0200

    Adding ComposeMessageWindow.fxml and ComposeMessageController.

---
+++ src/main/java/app/emailclient/controller/ComposeMessageController.java
@@ -0,0 +1,30 @@
+package app.emailclient.controller;
+
+import app.emailclient.EmailManager;
+import app.emailclient.view.ViewFactory;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.web.HTMLEditor;
+
+public class ComposeMessageController extends BaseController {
+    @FXML
+    private Label errorLabel;
+
+    @FXML
+    private HTMLEditor htmlEditor;
+
+    @FXML
+    private TextField recipientTxtField;
+
+    @FXML
+    private Button sendButton;
+
+    @FXML
+    private TextField subjectTxtField;
+
+    public ComposeMessageController(ViewFactory viewFactory, EmailManager emailManager, String fxmlName) {
+        super(viewFactory, emailManager, fxmlName);
+    }
+}

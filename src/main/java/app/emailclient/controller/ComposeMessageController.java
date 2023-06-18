5cd6424d9636daeb8644c8e1cd3f2530e956f815
Karol Programuje <karol341654@gmail.com>
2023-06-18 09:18:33 +0200

    EmailSenderService

--- src/main/java/app/emailclient/controller/ComposeMessageController.java
+++ src/main/java/app/emailclient/controller/ComposeMessageController.java
@@ -1,6 +1,7 @@
 package app.emailclient.controller;
 
 import app.emailclient.EmailManager;
+import app.emailclient.controller.services.EmailSenderService;
 import app.emailclient.model.EmailAccount;
 import app.emailclient.view.ViewFactory;
 import javafx.fxml.FXML;
@@ -39,7 +40,24 @@ public class ComposeMessageController extends BaseController implements Initiali
 
     @FXML
     void setOnSendButtonAction() {
+        EmailSenderService emailSenderService = new EmailSenderService(
+                accountChoiceBx.getValue(),
+                subjectTxtField.getText(),
+                recipientTxtField.getText(),
+                htmlEditor.getHtmlText()
+        );
 
+        emailSenderService.start();
+        emailSenderService.setOnSucceeded(e -> {
+            EmailSendingResult emailSendingResult = emailSenderService.getValue();
+
+            switch (emailSendingResult) {
+                case SUCCESS -> errorLabel.setText("Message Send!");
+                case FAILED_BY_PROVIDER -> errorLabel.setText("Sending message failed by provider.");
+                case FAILED_BY_UNEXPECTED_ERROR -> errorLabel.setText("An unexpected error occurred while sending");
+
+            }
+        });
     }
 
     @Override
---
+++ src/main/java/app/emailclient/controller/services/EmailSenderService.java
@@ -0,0 +1,30 @@
+package app.emailclient.controller.services;
+
+import app.emailclient.controller.EmailSendingResult;
+import app.emailclient.model.EmailAccount;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+
+public class EmailSenderService extends Service<EmailSendingResult> {
+    private EmailAccount emailAccount;
+    private String subject;
+    private String recipient;
+    private String content;
+
+    public EmailSenderService(EmailAccount emailAccount, String subject, String recipient, String content) {
+        this.emailAccount = emailAccount;
+        this.subject = subject;
+        this.recipient = recipient;
+        this.content = content;
+    }
+
+    @Override
+    protected Task<EmailSendingResult> createTask() {
+        return new Task<EmailSendingResult>() {
+            @Override
+            protected EmailSendingResult call() throws Exception {
+                return null;
+            }
+        };
+    }
+}
